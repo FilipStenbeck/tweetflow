cfaf4da38f5c9a62606aae3dbd1d75408bd4e1de
FilipStenbeck <filip.stenbeck@gmail.com>
2014-06-04 21:49:27 +0200

    added more tests

--- frontend/react/test/spec/test.js
+++ frontend/react/test/spec/test.js
@@ -1,6 +1,6 @@
 /* global describe, it */
 
-define(['animation'], function (animation) {
+define(['animation', 'appUtil'], function (animation, appUtil) {
   
  
   describe('Tweetflow', function() {
@@ -19,6 +19,17 @@ define(['animation'], function (animation) {
         var sample2 =animation.getCssName();
 
         sample1.should.not.equal(sample2);
+      });
+    });
+
+    describe('AppUtil', function() {
+      
+      it('It should parse url parameters', function() {
+        var paramObj = appUtil.getQueryParams('http://localhost:9000/?query=reactjs&interval=5000')
+  
+        paramObj['http://localhost:9000/?query'].should.equal('reactjs');
+        paramObj['interval'].should.equal('5000');
+
       });
     });
   });
