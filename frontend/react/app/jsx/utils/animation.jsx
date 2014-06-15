247a360e677e24d533b3d354f191b821168bf005
FilipStenbeck <filip.stenbeck@gmail.com>
2014-06-15 11:09:17 +0200

    added webfonts more animations and did some styling

--- frontend/react/app/jsx/utils/animation.jsx
+++ frontend/react/app/jsx/utils/animation.jsx
@@ -5,8 +5,13 @@ define([], function () {
 		'shake animated',
 		'pulse animated',
 		'tada animated',
-		'flip animated',
+		'flip animated', 		
 		'rubberBand animated',
+		'bounceIn animated',
+		'bounceInUp animated',
+		'bounceInDown animated',
+		'rotateIn animated',
+		'lightSpeedIn animated'
 	];
 	lastId = -1;
 	
@@ -14,13 +19,12 @@ define([], function () {
 		//Get a new animation css and make sure its a diffrent one than the last
     	getCssName : function () {
        		var newId = Math.floor(Math.random() * animations.length);
-        	if (newId !== lastId ) {
-          		lastId = newId; 
-      			return animations[lastId];
+        	while (newId === lastId ) {
+      			newId = Math.floor(Math.random() * animations.length);
         	}
-        	else {
-        		this.getCssName();
-        	}
+          	lastId = newId; 
+          	console.log("ids", newId, lastId)
+        	return animations[newId];
     	}	
 	}
 });
